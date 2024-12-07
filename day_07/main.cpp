9b6e4149bc9c08d9e2efd40285f6b3b93fdf7e4e
EcklerOChokola <105422409+EcklerOChokola@users.noreply.github.com>
2024-12-07 11:58:48 +0100

    Day 07 - Bridge Repair

---
+++ day_07/main.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+
+#define L unsigned long long
+
+using namespace std;
+
+int line_calibration(L result, vector<L> inputs)
+{
+	for(int i = 0; i < (1 << (inputs.size() - 1)); i++)
+	{
+		L temp = inputs[0];
+		for(int j = 1; j < inputs.size(); j++)
+			if(1 << (j - 1) & i) temp += inputs[j];
+			else temp *= inputs[j];
+		if(temp==result) return 1;
+	}
+	return 0;
+}
+
+int line_calibration_2(L result, vector<L> inputs)
+{
+	if(line_calibration(result, inputs)) return 1;
+	for(int i = 0; i < (1 << (2 * (inputs.size() - 1))); i++)
+	{
+		L temp = inputs[0];
+		for(int j = 1; j < inputs.size(); j++)
+		{
+			int op = ((3 << (2 * (j - 1))) & i) >> (2 * (j - 1));
+			if(!op) {
+				temp = 0;
+				break;
+			}
+			else if(op == 1) temp += inputs[j];
+			else if(op == 2) temp *= inputs[j];
+			else 
+			{
+				stringstream t;
+				t << temp << inputs[j];
+				t >> temp;
+			}
+		}
+		if(temp==result) return 1;
+	}
+	return 0;
+}
+
+int main(void)
+{
+	string line;
+	L part_1 = 0;
+	L part_2 = 0;
+	int count = 0;
+	while(getline(cin, line))
+	{
+		if(line.empty()) break;
+		string r = line.substr(0, line.find(": "));
+		line.erase(0, line.find(": ") + string(": ").length());
+		stringstream s(r);
+		L result;
+		s >> result;
+		stringstream i(line);
+		string tmp;
+		L t;
+		vector<L> inputs;
+		while(getline(i, tmp, ' '))
+		{
+			stringstream v(tmp);
+			v >> t;
+			inputs.push_back(t);
+		}
+		part_1 += line_calibration(result, inputs) * result;
+		part_2 += line_calibration_2(result, inputs) * result;
+		count++;
+	}
+	cout << part_1 << '\n';
+	cout << part_2 << '\n';
+	return 0;
+}
+
+
+
+
